5733cc4e35438597408485968dece61196a4c916
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-07-26 18:58:21 +0800

    fix: fix a config name error

--- lophine-server/src/main/java/fun/bm/lophine/config/modules/misc/RedStoneConfig.java
+++ lophine-server/src/main/java/fun/bm/lophine/config/modules/misc/RedStoneConfig.java
@@ -3,12 +3,15 @@ package fun.bm.lophine.config.modules.misc;
 import me.earthme.luminol.config.EnumConfigCategory;
 import me.earthme.luminol.config.IConfigModule;
 import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.config.flags.TransformedConfig;
 
 public class RedStoneConfig implements IConfigModule {
-    @ConfigInfo(baseName = "allow_skip_cooldown", comments =
+    @TransformedConfig(name = "allow_skip_cooldown", category = {"misc", "redstone"})
+    @ConfigInfo(baseName = "shears_rotate", comments =
             """
                     Allows you to use the Shears to right-click to rotate the block.""")
     public static boolean shears = false;
+
     @Override
     public EnumConfigCategory getCategory() {
         return EnumConfigCategory.MISC;
