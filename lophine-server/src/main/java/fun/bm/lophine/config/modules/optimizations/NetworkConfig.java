52b6803d689c10c87c62dcd107403bf9fe6c8b32
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-07-27 13:54:22 +0800

    feat: leaves disable packet limit

---
+++ lophine-server/src/main/java/fun/bm/lophine/config/modules/optimizations/NetworkConfig.java
@@ -0,0 +1,21 @@
+package fun.bm.lophine.config.modules.optimizations;
+
+import me.earthme.luminol.config.EnumConfigCategory;
+import me.earthme.luminol.config.IConfigModule;
+import me.earthme.luminol.config.flags.ConfigInfo;
+
+public class NetworkConfig implements IConfigModule {
+    @ConfigInfo(baseName = "unlimit-packet", comments = """
+                    Disable packet limit""")
+    public static boolean disablePacketLimit = true;
+
+    @Override
+    public EnumConfigCategory getCategory() {
+        return EnumConfigCategory.OPTIMIZATIONS;
+    }
+
+    @Override
+    public String getBaseName() {
+        return "network";
+    }
+}
